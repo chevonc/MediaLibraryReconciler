a995383c5c8a8dce8d8b8ff34459efbcab8ac490
chevon <chevonc@live.com>
2014-02-09 14:00:42 -0800

    initial commit

---
+++ MediaLibraryReconciler.ConsoleTestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Mp3Lib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLibraryReconciler.ConsoleTestApp
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string testSong = Path.Combine(string.Format("{0}\\TestFiles\\SkyJames.mp3", Directory.GetCurrentDirectory()));
+         Stopwatch sw = new Stopwatch();
+         sw.Restart();
+         Mp3File song = new Mp3File(testSong);
+         song.TagHandler.Album = "MyAlbum" + DateTime.Now.ToLongTimeString(); ;
+         song.Update();
+         sw.Stop();
+         Console.WriteLine(sw.Elapsed.TotalSeconds);
+         Mp3File song1 = new Mp3File(testSong);
+         Console.WriteLine(song1.TagHandler.Album);
+         Console.ReadKey();
+      }
+   }
+}
---
+++ MediaLibraryReconciler/Flags/MP3MetaMismatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLibraryReconciler.Flags
+{
+   enum MP3MetaMismatchOption
+   {
+      FileName,
+      Folder,
+      GrandparentFolder,
+      FileZoneIdentifier,
+      Title,
+      Artist,
+      Album,
+      Genre,
+      Picture
+   }
+
+   enum MP3MetaMismatchReason
+   {
+      Empty,
+      DoesNotMatch,
+      Blocked,
+      UrlFormatted,
+   }
+}
